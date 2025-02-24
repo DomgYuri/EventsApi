bb5bb4f3b0c8035b3be40c12646cb7d09673751e
Yuri Domingues <dominguesyuri777@gmail.com>
2025-02-24 19:50:56 -0300

    Created Exceptions SubscriptionConflictException and UserIndicatorNotFound

---
+++ src/main/java/br/com/nwl/events/exception/UserIndicatorNotFound.java
@@ -0,0 +1,7 @@
+package br.com.nwl.events.exception;
+
+public class UserIndicatorNotFound extends RuntimeException {
+    public UserIndicatorNotFound(String message) {
+        super(message);
+    }
+}
